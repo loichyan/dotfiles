07807018e5c0e20a4bdd8fb37a11b93c785ab340
loichyan <loichyan@foxmail.com>
2022-06-30 22:02:55 +0800

    feat(packages): allow `packages.default` to build all packages

--- packages/flake.nix
+++ packages/flake.nix
@@ -10,13 +10,20 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
         packages = {
           symbols-nerd-font = pkgs.callPackage ./symbols-nerd-font { };
           nix-single-user = pkgs.callPackage ./nix-single-user { };
           akmods-keys = pkgs.callPackage ./akmods-keys { };
         };
+      in
+      {
+        packages = {
+          default =
+            pkgs.buildEnv {
+              name = "my-packages";
+              paths = builtins.attrValues packages;
+            };
+        } // packages;
       }
     )
   ;
