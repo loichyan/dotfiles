ce2acf7428205a38ee465d76747d449b6f74fb25
loichyan <loichyan@foxmail.com>
2023-01-28 12:51:40 +0800

    fix(scripts): add missing '\'

--- scripts/fedora_pkgs.sh
+++ scripts/fedora_pkgs.sh
@@ -2,7 +2,7 @@
 
 # Import RPM Fusion
 rpm-ostree install \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 #  Install Nvidia driver and setup secure boot.
 rpm-ostree install akmod-nvidia
