2055446e1db31f74eb37142f7ceefe322cf5ea7a
loichyan <loichyan@foxmail.com>
2023-03-12 16:00:47 +0800

    feat(nvim): improve cmp items formatting

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -1,10 +1,41 @@
+local Config = require("deltavim.config")
 local Util = require("deltavim.util")
 
 return {
   {
     "nvim-cmp",
     opts = function(_, opts)
+      local source_names = {
+        buffer = "[BUF]",
+        creates = "[CRATES]",
+        luasnip = "[SNIP]",
+        nvim_lsp = "[LSP]",
+        path = "[PATH]",
+      }
+      local source_dups = {
+        buffer = 1,
+        luasnip = 1,
+        nvim_lsp = 0,
+        path = 1,
+      }
+      local icons = Config.icons.kinds
       return Util.merge({}, opts, {
+        formatting = {
+          fields = { "abbr", "kind", "menu" },
+          format = function(entry, item)
+            local source = entry.source.name
+            local max_width = math.floor(vim.o.columns * 0.4)
+            if #item.abbr > max_width then
+              item.abbr = string.sub(item.abbr, 1, max_width) .. "..."
+            end
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+            item.menu = source_names[source]
+            item.dup = source_dups[source] or 0
+            return item
+          end,
+        },
         sources = require("cmp").config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" },
