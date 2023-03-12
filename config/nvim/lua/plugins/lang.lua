79ead4c40ada5a7e4be2861076b115be612db3ef
loichyan <loichyan@foxmail.com>
2023-03-12 16:01:07 +0800

    feat(nvim): add `rust-tools.nvim`

--- config/nvim/lua/plugins/lang.lua
+++ config/nvim/lua/plugins/lang.lua
@@ -5,8 +5,20 @@ return {
   { "b0o/schemastore.nvim", lazy = true },
   -- Rust
   {
+    "simrat39/rust-tools.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      tools = {
+        inlay_hints = { auto = false },
+      },
+      server = {
+        standalone = false,
+      },
+    },
+  },
+  {
     "Saecki/crates.nvim",
-    event = { "BufReadPre", "BufNewFile" },
+    event = { "BufReadPost", "BufNewFile" },
     opts = {
       popup = { border = "rounded" },
       null_ls = {
