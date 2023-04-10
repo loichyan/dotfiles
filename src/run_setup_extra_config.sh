4e6d9d33c2577bf1ccac75ef95f1f89e87d60a22
loichyan <loichyan@foxmail.com>
2023-04-10 10:02:23 +0800

    feat(chezmoi): setup rime configuration

---
+++ src/run_setup_extra_config.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+root=$(realpath "$CHEZMOI_SOURCE_DIR/..")
+
+warn() {
+  printf "\e[33m[WARN]\e[0m %s\n" "$1"
+}
+
+info() {
+  printf "\e[36m[INFO]\e[0m %s\n" "$1"
+}
+
+symlink() {
+  src="$root/$1"
+  dest="$HOME/$2"
+
+  if [ ! -e "$src" ]; then
+    warn "Skip non-existent path '$src'"
+    return
+  elif [ -h "$dest" ]; then
+    rm "$dest"
+  elif [ -e "$dest" ]; then
+    warn "Skip existing path '$dest'"
+    return
+  fi
+
+  ln -s "$src" "$dest"
+  info "Create symlink '$dest'"
+}
+
+info "Setup extra configuration"
+
+# symlink directories
+symlink nvim .config/nvim
+symlink private/ssh .ssh
+
+if [ ! -d "$HOME/.plum" ]; then
+  info "Install rime/plum"
+  git clone --depth 1 "https://github.com/rime/plum" ~/.plum
+  mkdir -p ~/.local/bin
+  ln -s ~/.plum/rime-install ~/.local/bin/rime-install
+  info "Clone iDvel/rime-ice"
+  bash ~/.plum/rime-install iDvel/rime-ice:others/recipes/full
+fi
