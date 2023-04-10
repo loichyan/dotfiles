a8ae3daa1aa33b501c5fb59dd7a4391e73e58ded
loichyan <loichyan@foxmail.com>
2023-04-10 09:25:49 +0800

    style(sh): format with `shfmt`

--- src/run_symlink_dirs.sh
+++ src/run_symlink_dirs.sh
@@ -3,29 +3,29 @@
 root=$(realpath "$CHEZMOI_SOURCE_DIR/..")
 
 warn() {
-	printf "\e[33m[WARN]\e[0m %s\n" "$1"
+  printf "\e[33m[WARN]\e[0m %s\n" "$1"
 }
 
 info() {
-	printf "\e[36m[INFO]\e[0m %s\n" "$1"
+  printf "\e[36m[INFO]\e[0m %s\n" "$1"
 }
 
 symlink() {
-	src="$root/$1"
-	dest="$HOME/$2"
+  src="$root/$1"
+  dest="$HOME/$2"
 
-	if [ ! -e "$src" ]; then
-		warn "skip non-existent path '$src'"
-		return
-	elif [ -h "$dest" ]; then
-		rm "$dest"
-	elif [ -e "$dest" ]; then
-		warn "skip existing path '$dest'"
-		return
-	fi
+  if [ ! -e "$src" ]; then
+    warn "skip non-existent path '$src'"
+    return
+  elif [ -h "$dest" ]; then
+    rm "$dest"
+  elif [ -e "$dest" ]; then
+    warn "skip existing path '$dest'"
+    return
+  fi
 
-	ln -s "$src" "$dest"
-	info "create symlink '$dest'"
+  ln -s "$src" "$dest"
+  info "create symlink '$dest'"
 }
 
 symlink nvim .config/nvim
