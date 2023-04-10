163a5e9649e82d3047ec2a8b8fe2ed073dd24a38
loichyan <loichyan@foxmail.com>
2023-04-10 09:51:02 +0800

    fix(sh): change shebang to `#!/usr/bin/env bash`

--- src/dot_config/home-manager/services.nix
+++ src/dot_config/home-manager/services.nix
@@ -3,7 +3,7 @@ let
   inherit (pkgs) myData xray aria tor writeScript;
   geodat = writeScript "geodat"
     ''
-      #!/bin/sh
+      #!/usr/bin/env bash
       mkdir -p ${myData.home}/.local/share/xray
       curl -L https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -o ${myData.home}/.local/share/xray/geoip.dat
       curl -L https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -o ${myData.home}/.local/share/xray/geosite.dat
--- src/dot_tmux/tmux-nova.tmux
+++ src/dot_tmux/tmux-nova.tmux
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # Name:     Catppuccin Mocha
 # Author:   Catppuccin Org
