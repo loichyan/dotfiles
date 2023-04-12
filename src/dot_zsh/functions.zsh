1aaa4b7fa5fb745704a5c911c82a74f3cca4a5d4
loichyan <loichyan@foxmail.com>
2023-04-12 10:05:12 +0800

    feat(zsh): add functions to backup and restore volumes

--- src/dot_zsh/functions.zsh
+++ src/dot_zsh/functions.zsh
@@ -23,3 +23,33 @@ byproxy() {
     https_proxy="$MY_HTTP_PROXY" \
     "$@"
 }
+
+# Backup a volume
+docker-vbackup() {
+  if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Directroy/volume supplied"
+    return 1
+  fi
+  local dir=$1
+  local vname=$2
+  docker run --rm \
+    -v "$vname:/$vname" \
+    -v "$(pwd)/$dir:/__backup:Z" \
+    alpine \
+    sh -c "tar -C '/$vname' -cvf '/__backup/$vname.tar.gz' ."
+}
+
+# Restore a volume
+docker-vrestore() {
+  if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Directroy/volume supplied"
+    return 1
+  fi
+  local dir=$1
+  local vname=$2
+  docker run --rm \
+    -v "$vname:/$vname" \
+    -v "$(pwd)/$dir:/__backup:Z" \
+    alpine \
+    sh -c "tar -C '/$vname' -xvf '/__backup/$vname.tar.gz'"
+}
