f600ee037c2f91e2cccaa88c918f26fbcc1ed30a
loichyan <loichyan@foxmail.com>
2023-04-18 09:43:37 +0800

    fix(zsh): set path only once

--- src/dot_zsh/env.zsh
+++ src/dot_zsh/env.zsh
@@ -1,5 +1,5 @@
 # Remove duplicated entries in PATH.
-typeset -U PATH path
+typeset -TUx PATH path
 
 # Set default editor.
 export EDITOR=nvim
@@ -7,30 +7,3 @@ export VISUAL=nvim
 
 # Define python user base folder.
 export PYTHONUSERBASE=~/.pip
-
-# Setup environment variables for Nix and Home Manager.
-for profile (
-  ~/.nix-profile/etc/profile.d/nix.sh
-  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-) do
-  if [[ -e $profile ]]; then
-    source $profile
-  fi
-done
-unset profile
-
-# Language specified packages.
-declare -A local_path=(
-  [cargo]=~/.cargo/bin
-  [pip]=~/.pip/bin
-  [pnpm]=~/.pnpm/bin
-)
-for k v in ${(kv)local_path}; do
-  if (( ${+commands[$k]} )); then
-    path=( $v $path )
-  fi
-done
-unset local_path k v
-
-# Local installed packages.
-path=( ~/.local/bin $path )
