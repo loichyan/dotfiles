bcd2988f92efe8987e87fb0543095ebfc02e8ba1
loichyan <loichyan@foxmail.com>
2023-05-03 09:16:39 +0800

    feat(nixpkgs): add `templates/rust-bin`

---
+++ src/dot_config/nixpkgs/templates/rust-bin/flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, fenix, crane, ... }:
+    let
+      mkPkgs = pkgs:
+        rec {
+          rustToolchain = (with pkgs.fenix; combine [
+            stable.defaultToolchain
+            stable.rust-src
+          ]);
+          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+          myCrate = craneLib.buildPackage {
+            src = craneLib.cleanCargoSource (craneLib.path ./.);
+          };
+        }
+      ;
+    in
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
+        inherit (mkPkgs pkgs) rustToolchain myCrate;
+      in
+      {
+        packages.default = myCrate;
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            rustToolchain
+          ];
+        };
+      }
+    )) // {
+      overlays.default = _: prev: {
+        inherit (mkPkgs prev) myCrate;
+      };
+    }
+  ;
+}
