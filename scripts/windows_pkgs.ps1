332925e623d6343ea880af847d39f160c8042a8a
loichyan <loichyan@foxmail.com>
2023-06-02 09:55:37 +0800

    feat(windows_pkgs): update packages

--- scripts/windows_pkgs.ps1
+++ scripts/windows_pkgs.ps1
@@ -1,15 +1,10 @@
-# Install Scoop package manager.
-Invoke-Expression (New-Object net.webclient).downloadstring('https://get.scoop.sh')
-
 # Install packages for development.
-scoop install 7zip gsudo git gcc make python neovim nodejs yarn
-winget install -i Microsoft.PowerShell
+winget install -i  7zip.7zip
 winget install -i Microsoft.VisualStudioCode
 
 # Install packages for media.
 winget install -i Daum.PotPlayer
 winget install -i Netease.CloudMusic
-winget install -i ByteDance.JianyingPro
 
 # Install other packages.
 winget install -i voidtools.Everything.Lite
