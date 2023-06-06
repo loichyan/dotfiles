a3766b86d25cdf558e17ea30a029eda33bd33172
loichyan <loichyan@foxmail.com>
2023-06-06 10:31:47 +0800

    fix(zsh): check packages before install zimfw

--- src/dot_zsh/config.zsh
+++ src/dot_zsh/config.zsh
@@ -42,12 +42,15 @@ ZVM_INIT_MODE=sourcing
 #
 
 ZIM_HOME=~/.zim
-# Download zimfw plugin manager if missing.
 if [[ ! -e "${ZIM_HOME}/zimfw.zsh" ]]; then
-  echo "Download zimfw plugin manager"
-  curl -fsSL --create-dirs -o "${ZIM_HOME}/zimfw.zsh" \
-    https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+  echo "Initialize zimfw plugin manager"
+  if (( ! ${+commands[git]} || ! ${+commands[curl]} )); then
+    echo "git and curl is required for zimfw"
+    return 1
+  fi
+  curl -fsSL --create-dirs -o "${ZIM_HOME}/zimfw.zsh" "https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh"
 fi
+
 # Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.
 if [[ ! "${ZIM_HOME}/init.zsh" -nt "${ZDOTDIR:-${HOME}}/.zimrc" ]]; then
   source "${ZIM_HOME}/zimfw.zsh" init
