e18ed394b26ef57d12e67f95f0400695c85df766
loichyan <loichyan@foxmail.com>
2023-06-06 13:52:38 +0800

    feat(windows_pkgs): add aria2

--- scripts/windows_pkgs.ps1
+++ scripts/windows_pkgs.ps1
@@ -1,5 +1,5 @@
 # Install packages for development.
-winget install -i  7zip.7zip
+winget install -i 7zip.7zip
 winget install -i Microsoft.VisualStudioCode
 
 # Install packages for media.
@@ -8,5 +8,6 @@ winget install -i Netease.CloudMusic
 
 # Install other packages.
 winget install -i voidtools.Everything.Lite
+winget install -i aria2.aria2
 winget install -i Tencent.WeChat
 winget install -i Tencent.QQ
