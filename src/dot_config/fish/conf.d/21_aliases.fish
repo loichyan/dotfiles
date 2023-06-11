2af45d96929cb3e2fa0172da17818b625abb71df
loichyan <loichyan@foxmail.com>
2023-06-11 15:49:54 +0800

    fix(fish): recursive function calls

--- src/dot_config/fish/conf.d/21_aliases.fish
+++ src/dot_config/fish/conf.d/21_aliases.fish
@@ -2,5 +2,5 @@ if status is-interactive
     type -q lsd && alias ls lsd
     type -q lazygit && alias lg lazygit
     type -q safe-rm && alias rm safe-rm
-    test "$TERM_PROGRAM" = WezTerm && alias nvim "TERM=wezterm nvim"
+    type -q nvim && test "$TERM_PROGRAM" = WezTerm && alias nvim (printf "TERM=wezterm %s" (which nvim))
 end
