41e2770b5e8d313be8ada299ad90ebad7e7ccf8c
loichyan <loichyan@foxmail.com>
2023-06-11 19:10:08 +0800

    fix(fish): no need to set term for neovim

--- src/dot_config/fish/conf.d/21_aliases.fish
+++ src/dot_config/fish/conf.d/21_aliases.fish
@@ -2,5 +2,4 @@ if status is-interactive
     type -q lsd && alias ls lsd
     type -q lazygit && alias lg lazygit
     type -q safe-rm && alias rm 'safe-rm -i' || alias rm (printf '%s -i' (which rm))
-    type -q nvim && test "$TERM_PROGRAM" = WezTerm && alias nvim (printf 'TERM=wezterm %s' (which nvim))
 end
