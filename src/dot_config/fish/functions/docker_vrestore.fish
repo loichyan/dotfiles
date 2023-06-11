71902b9a4b3fb5033bdfd59dc0233663dedd9e21
loichyan <loichyan@foxmail.com>
2023-06-11 16:14:49 +0800

    fix(fish): bad docker volume backup/restore functions

--- src/dot_config/fish/functions/docker_vrestore.fish
+++ src/dot_config/fish/functions/docker_vrestore.fish
@@ -1,12 +1,12 @@
 function docker_vrestore
     test -n "$argv" && argparse f/file= v/volume= -- $argv || return
     set -l file (realpath $_flag_f)
-    set -l dir (dirname $path)
-    set -l base (basename $path)
+    set -l dir (dirname $file)
+    set -l base (basename $file)
     set -l volume $_flag_v
     docker run --rm \
+        -v $volume:/tmp/volume:Z \
         -v $dir:/tmp/backup:ro,Z \
-        -v $volume:/tmp/volume \
         alpine \
-        sh -c "tar -C '/tmp/volume' -xvf '/tmp/backup/$base'"
+        sh -c "tar -C '/tmp/volume' -xvzf '/tmp/backup/$base'"
 end
