18dfee43fcf0030f2c12a808b6ca2172f264c792
loichyan <loichyan@foxmail.com>
2023-06-12 08:25:25 +0800

    fix(fish): `rm -i` is too annoying

--- src/dot_config/fish/conf.d/21_aliases.fish
+++ src/dot_config/fish/conf.d/21_aliases.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     type -q lsd && alias ls lsd
     type -q lazygit && alias lg lazygit
-    type -q safe-rm && alias rm 'safe-rm -i' || alias rm (printf '%s -i' (which rm))
+    type -q safe-rm && alias rm 'safe-rm -I' || alias rm (printf '%s -I' (which rm))
 end
