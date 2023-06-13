0ece5a37ece126ff881917779b70de88432ffff2
loichyan <loichyan@foxmail.com>
2023-06-13 10:09:47 +0800

    fix(fish): set complete and function path when status is interactive

--- src/dot_config/fish/conf.d/10_path.fish
+++ src/dot_config/fish/conf.d/10_path.fish
@@ -1,16 +1,20 @@
+set -l nix_profile ~/.nix-profile
+
 if status is-login
    # Setup environment variables for Nix and Home Manager.
-   set -l nix_profile ~/.nix-profile
    for profile in \
        $nix_profile/etc/profile.d/nix.fish \
        $nix_profile/etc/profile.d/hm-session-vars.fish
        test -f $profile && source $profile
    end
 
+   # Local installed packages.
+   fish_add_path -gp ~/.local/bin ~/.cargo/bin ~/.pip/bin ~/.pnpm/bin
+
+end
+
+if status is-interactive
    # Vendor completions and functions
    set -ga fish_complete_path $nix_profile/share/fish/vendor_completions.d
    set -ga fish_function_path $nix_profile/share/fish/vendor_functions.d
-
-   # Local installed packages.
-   fish_add_path -gp ~/.local/bin ~/.cargo/bin ~/.pip/bin ~/.pnpm/bin
 end
