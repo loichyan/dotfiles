9cb448db564d19a225671345c08e308b44470c86
loichyan <loichyan@foxmail.com>
2023-06-13 17:43:54 +0800

    fix(fish): use full path in aliases

--- src/dot_config/fish/conf.d/21_aliases.fish
+++ src/dot_config/fish/conf.d/21_aliases.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     function alias_command
-        ! type -qf $argv[1] || alias $argv[1] "command $argv[2..-1]"
+        ! set -l cmd (type -fp $argv[2]) || alias $argv[1] "$cmd $argv[3..-1]"
     end
 
     alias_command ls lsd
