f29b2d9bcaf509a589f7271b5da4779bbfdb2776
loichyan <loichyan@foxmail.com>
2023-06-14 10:43:35 +0800

    fix(nvim): better appearance in WezTerm

--- src/dot_config/fish/conf.d/21_aliases.fish
+++ src/dot_config/fish/conf.d/21_aliases.fish
@@ -1,10 +1,12 @@
 if status is-interactive
     function alias_command
-        ! set -l cmd (type -fp $argv[2]) || alias $argv[1] "$cmd $argv[3..-1]"
+        argparse -N 2 p/prefix=+ s/suffix=+ -- $argv || return
+        ! set -l cmd (type -fp $argv[2]) || alias $argv[1] "$_flag_prefix $cmd $_flag_suffix $argv[3..-1]"
     end
 
     alias_command ls lsd
     alias_command lg lazygit
-    alias_command rm rm -I
-    alias_command rm safe-rm -I
+    alias_command rm rm -- -I
+    alias_command rm safe-rm -- -I
+    [ "$TERM_PROGRAM" != WezTerm ] || alias_command nvim nvim -p TERM=wezterm
 end
