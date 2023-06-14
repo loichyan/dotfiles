b745b818c69bdf4d3cef61879731abb18ff4a134
loichyan <loichyan@foxmail.com>
2023-06-14 10:46:24 +0800

    refactor(fish): full flag name

--- src/dot_config/fish/functions/docker_vrestore.fish
+++ src/dot_config/fish/functions/docker_vrestore.fish
@@ -1,9 +1,9 @@
 function docker_vrestore
     [ -n "$argv" ] && argparse f/file= v/volume= -- $argv || return
-    set -l file (realpath $_flag_f)
+    set -l file (realpath $_flag_file)
     set -l dir (dirname $file)
     set -l base (basename $file)
-    set -l volume $_flag_v
+    set -l volume $_flag_volume
     set -l docker (type -q podman && echo podman || echo docker)
     $docker run --rm \
         -v $volume:/tmp/volume \
