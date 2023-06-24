84089c966c2e0a9ab874fae91a87da3a4f5ba2c9
loichyan <loichyan@foxmail.com>
2023-06-24 16:54:53 +0800

    fix(fish): overwrite error status

--- src/dot_config/fish/functions/setproxy.fish
+++ src/dot_config/fish/functions/setproxy.fish
@@ -1,9 +1,5 @@
 function setproxy
-    if [ -n "$argv" ]
-        set -f proxy $argv[1]
-    else
-        set -f proxy $MY_HTTP_PROXY
-    end
+    set -f proxy ([ -n "$argv" ] && echo $argv[1] || echo $MY_HTTP_PROXY)
     set -gx HTTP_PROXY $proxy
     set -gx http_proxy $proxy
     set -gx HTTPS_PROXY $proxy
