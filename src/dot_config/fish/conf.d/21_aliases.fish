2e51fc2974e8dcd21137b1821987dccfb4dd8b37
loichyan <loichyan@foxmail.com>
2023-07-07 14:39:32 +0800

    feat(lazygit): integrate with nvim

--- src/dot_config/fish/conf.d/21_aliases.fish
+++ src/dot_config/fish/conf.d/21_aliases.fish
@@ -3,11 +3,13 @@ if status is-interactive
         ! set -l cmd (type -fp $argv[2]) || alias $argv[1] "$prefix $cmd $argv[3..]"
     end
 
+    set -l prefix
     alias_command ls lsd
     alias_command lg lazygit
     alias_command rm rm -I
     alias_command rm safe-rm -I
-    [ "$TERM_PROGRAM" != WezTerm ] || prefix='TERM=wezterm' alias_command nvim nvim -p
+    [ "$TERM_PROGRAM" != WezTerm ] || set prefix 'TERM=wezterm'
+    alias_command nvim nvim --listen /tmp/nvim-server.sock
 
     functions -e alias_command
 end
