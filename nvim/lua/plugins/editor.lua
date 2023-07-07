4aaee610d3c7b990637ecc2c68c96467f9bd670d
loichyan <loichyan@foxmail.com>
2023-07-07 14:48:11 +0800

    feat(nvim): scroll diffview with Ctrl+U/D

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -153,12 +153,17 @@ return {
         :collect_lazy()
     end,
     opts = function()
+      local actions = require("diffview.actions")
       -- stylua: ignore
       local close = { "n", "q", "<Cmd>DiffviewClose<CR>", { desc = "Close diffview" } }
       return {
         keymaps = {
           view = { close },
-          file_panel = { close },
+          file_panel = {
+            close,
+            { "n", "<c-u>", actions.scroll_view(-0.25), { desc = "Scroll the view up" } },
+            { "n", "<c-d>", actions.scroll_view(0.25), { desc = "Scroll the view down" } },
+          },
           file_history_panel = { close },
         },
       }
