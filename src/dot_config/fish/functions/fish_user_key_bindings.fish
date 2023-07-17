d58bd5cd49f235658ff260b37a4d09b699db6ede
loichyan <loichyan@foxmail.com>
2023-07-17 17:05:06 +0800

    fix(fish): keep the default CTRL+E binding

--- src/dot_config/fish/functions/fish_user_key_bindings.fish
+++ src/dot_config/fish/functions/fish_user_key_bindings.fish
@@ -1,7 +1,6 @@
 function fish_user_key_bindings
     fish_default_key_bindings -M insert
     fish_vi_key_bindings --no-erase insert
-    bind -M insert \ce forward-word
     bind -M insert \cw backward-kill-word
     bind -M insert JK (bind -M insert \e | string match -r '\'(.*)\'$')[2]
 end
