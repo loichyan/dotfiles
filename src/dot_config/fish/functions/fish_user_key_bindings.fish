c1487f1a0684695732af2109f68144d65d544140
loichyan <loichyan@foxmail.com>
2023-07-17 17:34:52 +0800

    feat(fish): use CTRL+F/B to modify completion

--- src/dot_config/fish/functions/fish_user_key_bindings.fish
+++ src/dot_config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,7 @@
 function fish_user_key_bindings
     fish_default_key_bindings -M insert
     fish_vi_key_bindings --no-erase insert
-    bind -M insert \cw backward-kill-word
+    bind -M insert \cb backward-kill-word
+    bind -M insert \cf forward-word
     bind -M insert JK (bind -M insert \e | string match -r '\'(.*)\'$')[2]
 end
