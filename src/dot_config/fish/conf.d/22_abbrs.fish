f6703ec9506ece35df9559270b54e3b28b2ed3a2
loichyan <loichyan@foxmail.com>
2023-07-21 14:53:01 +0800

    pref(fish): reduce command checks

--- src/dot_config/fish/conf.d/22_abbrs.fish
+++ src/dot_config/fish/conf.d/22_abbrs.fish
@@ -1,59 +1,65 @@
 if status is-interactive
-    function abbr_command
-        ! type -q $argv[2] || abbr -a $argv[1] $argv[2..]
-    end
-
     function abbr_cargo
-        abbr_command "C$argv[1]" cargo $argv[2..]
+        abbr "C$argv[1]" cargo $argv[2..]
     end
 
     function abbr_docker
-        abbr_command "D$argv[1]" docker $argv[2..]
-        abbr_command "P$argv[1]" podman $argv[2..]
+        abbr "$D$argv[1]" $docker $argv[2..]
     end
 
     function abbr_docker_compose
-        abbr_command "Dc$argv[1]" docker-compose $argv[2..]
-        abbr_command "Pc$argv[1]" podman-compose $argv[2..]
+        abbr "$D"c"$argv[1]" "$docker"-compose $argv[2..]
     end
 
     function abbr_git
-        abbr_command "G$argv[1]" git $argv[2..]
+        abbr "G$argv[1]" git $argv[2..]
     end
 
-    abbr_cargo
-    abbr_cargo a add
-    abbr_cargo ab add --build
-    abbr_cargo ad add --dev
-    abbr_cargo b build
-    abbr_cargo br build --release
-    abbr_cargo c clippy
-    abbr_cargo r run
-    abbr_cargo rr run --release
-    abbr_cargo t test
+    if type -q cargo
+        abbr_cargo
+        abbr_cargo a add
+        abbr_cargo ab add --build
+        abbr_cargo ad add --dev
+        abbr_cargo b build
+        abbr_cargo br build --release
+        abbr_cargo c clippy
+        abbr_cargo r run
+        abbr_cargo rr run --release
+        abbr_cargo t test
+    end
 
-    abbr_docker
-    abbr_docker c compose
-    abbr_docker i image
-    abbr_docker il image ls
-    abbr_docker r run -it --rm
+    set -l cmds
+    ! type -q docker || set -a cmds docker,D
+    ! type -q podman || set -a cmds podman,P
+    for cmd in $cmds
+        echo $cmd | read -d , docker D
+        docker=$docker D=$D begin
+            type -q "$docker"-compose || alias "$docker"-compose "$docker compose"
+            abbr_docker
+            abbr_docker c compose
+            abbr_docker i image
+            abbr_docker il image ls
+            abbr_docker r run -it --rm
+            abbr_docker_compose
+            abbr_docker_compose l logs --tail=30 -f
+            abbr_docker_compose r restart
+        end
+    end
 
-    abbr_docker_compose
-    abbr_docker_compose l logs --tail=30 -f
-    abbr_docker_compose r restart
-
-    abbr_git
-    abbr_git c commit
-    abbr_git f fetch
-    abbr_git l log
-    abbr_git m merge
-    abbr_git mc merge --continue
-    abbr_git p push
-    abbr_git r rebase
-    abbr_git rc rebase --continue
-    abbr_git s status
-    abbr_git ck checkout
-    abbr_git sw checkout
+    if type -q git
+        abbr_git
+        abbr_git c commit
+        abbr_git f fetch
+        abbr_git l log
+        abbr_git m merge
+        abbr_git mc merge --continue
+        abbr_git p push
+        abbr_git r rebase
+        abbr_git rc rebase --continue
+        abbr_git s status
+        abbr_git ck checkout
+        abbr_git sw checkout
+    end
 
     functions -e abbr_{command,docker,compose,git}
 end
