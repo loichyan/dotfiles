17d1b0f38b4eb79b336c0d8562be7dbecbed3ba1
loichyan <loichyan@foxmail.com>
2023-08-06 12:07:03 +0800

    feat(fish): protect interrupted commands

--- src/dot_config/fish/conf.d/99_hooks.fish
+++ src/dot_config/fish/conf.d/99_hooks.fish
@@ -1,36 +1,29 @@
 if status is-interactive
     set -g __history_protected $history
+    set -g __history_protected_this 0
     set -g __history_failed
     set -g __history_deletions
-    set -g history_ignored bat cat cd ch{con,grp,mod,own} cp echo l{s,l} math mv history printf rm string
+    set -g history_ignored bat cat cd ch{con,grp,mod,own} \
+        cp echo l{s,l} mkdir mv history printf rm sudo
 
-    function ADD -d "Protected a history entry from deletion."
-        echo $argv
+    function __hook_fish_preexec -e fish_preexec
+        set -g __history_protected_this 0
     end
 
-    function DEL -d "Delete a history from histories."
-        echo $argv
+    function __hook_sigint -s SIGINT
+        # Protect interrupted command.
+        set -g __history_protected_this 1
     end
 
-
-    function __hook_record_histories -e fish_postexec
-        if set -l mat (string match -r '(ADD|DEL) (.+)' $argv[1])
-            set -l entry (eval "echo $mat[3]")
-            switch $mat[2]
-                case ADD
-                    set -a __history_protected $entry
-                case DEL
-                    set -a __history_deletions $entry
-            end
-        else if test $status -eq 0
+    function __hook_fish_postexec -e fish_postexec
+        if [ "$status" = 0 ] || [ "$__history_protected_this" = 1 ]
             set -a __history_protected $argv[1]
         else
             set -a __history_failed $argv[1]
         end
     end
 
-    function __hook_delete_histories -e fish_exit
-        set -a history_ignored ADD DEL
+    function __hook_fish_exit -e fish_exit
         set -a __history_deletions $history_ignored
         for entry in $__history_failed
             if ! contains $entry $__history_protected
