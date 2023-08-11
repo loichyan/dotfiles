24187afb9dc576144e4f95d89dd5d20c6ebe51cc
loichyan <loichyan@foxmail.com>
2023-08-11 08:05:08 +0800

    feat(fedora_pkgs): add `pop-shell`

--- scripts/fedora_pkgs.sh
+++ scripts/fedora_pkgs.sh
@@ -53,6 +53,7 @@ gsettings get org.gnome.software download-updates
 rpm-ostree install \
   fish wezterm \
   gnome-tweaks yaru-theme ibus-rime \
+  gnome-shell-extension-pop-shell pop-launcher qalculate \
   xray v2raya \
   cascadia-code-fonts sarasa-gothic-fonts symbols-nerd-fonts \
   podman-compose wl-clipboard xclip \
