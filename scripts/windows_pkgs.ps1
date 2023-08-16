315a56bf694745276aa2237cfec4707df0c5e5b6
loichyan <loichyan@foxmail.com>
2023-08-16 20:44:41 +0800

    feat(windows_pkgs): add some packages

--- scripts/windows_pkgs.ps1
+++ scripts/windows_pkgs.ps1
@@ -1,6 +1,8 @@
 # Install packages for development.
 winget install -i 7zip.7zip
 winget install -i Microsoft.VisualStudioCode
+winget install -i gsudo
+winget install -i Microsoft.PowerShell
 
 # Install packages for media.
 winget install -i Daum.PotPlayer
@@ -8,6 +10,5 @@ winget install -i Netease.CloudMusic
 
 # Install other packages.
 winget install -i voidtools.Everything.Lite
-winget install -i aria2.aria2
 winget install -i Tencent.WeChat
 winget install -i Tencent.QQ
