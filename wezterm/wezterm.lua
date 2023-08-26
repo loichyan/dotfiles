5531565cfe6bee347d4c3b2af9ade5cb5c0b487b
loichyan <loichyan@foxmail.com>
2023-08-26 15:24:04 +0800

    feat(wezterm): improve default search mode keybindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,7 +13,35 @@ local function toggle_domain(name)
     end
   end)
 end
--- 牛逼哈哈昂
+
+local key_tables = W.gui.default_key_tables()
+local accept_pattern = Act.Multiple({
+  Act.CopyMode("ClearSelectionMode"),
+  Act.CopyMode("AcceptPattern"),
+})
+local clear_pattern = Act.Multiple({
+  Act.CopyMode("ClearPattern"),
+  Act.CopyMode("ClearSelectionMode"),
+  Act.CopyMode("AcceptPattern"),
+})
+local my_key_tables = {
+  copy_mode = {
+    { key = "/", action = Act.Search({ CaseInSensitiveString = "" }) },
+    { key = "n", action = Act.CopyMode("NextMatch") },
+    { key = "n", mods = "SHIFT", action = Act.CopyMode("PriorMatch") },
+    { key = "c", mods = "CTRL", action = clear_pattern },
+  },
+  search_mode = {
+    { key = "Enter", action = accept_pattern },
+    { key = "c", mods = "CTRL", action = clear_pattern },
+  },
+}
+for k, list in pairs(my_key_tables) do
+  for _, kb in ipairs(list) do
+    table.insert(key_tables[k], kb)
+  end
+end
+
 return {
   -- Multiplexer
   unix_domains = {
@@ -54,98 +82,28 @@ return {
   -- Keybindings
   disable_default_key_bindings = true,
   mouse_bindings = {
-    {
-      event = { Up = { streak = 1, button = "Left" } },
-      mods = "NONE",
-      action = Act.Nop,
-    },
+    { event = { Up = { streak = 1, button = "Left" } }, action = Act.Nop },
   },
   keys = {
-    {
-      key = "c",
-      mods = "CTRL|SHIFT",
-      action = Act.CopyTo("Clipboard"),
-    },
-    {
-      key = "v",
-      mods = "CTRL|SHIFT",
-      action = Act.PasteFrom("Clipboard"),
-    },
-    {
-      key = "c",
-      mods = "ALT",
-      action = Act.CopyTo("PrimarySelection"),
-    },
-    {
-      key = "v",
-      mods = "ALT",
-      action = Act.PasteFrom("PrimarySelection"),
-    },
-    {
-      key = "m",
-      mods = "CTRL|SHIFT",
-      action = Act.TogglePaneZoomState,
-    },
-    {
-      key = "n",
-      mods = "CTRL|SHIFT",
-      action = Act.SpawnTab("CurrentPaneDomain"),
-    },
-    {
-      key = "|",
-      mods = "CTRL|SHIFT",
-      action = Act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-    },
-    {
-      key = "_",
-      mods = "CTRL|SHIFT",
-      action = Act.SplitVertical({ domain = "CurrentPaneDomain" }),
-    },
-    {
-      key = "o",
-      mods = "CTRL|SHIFT",
-      action = Act.RotatePanes("Clockwise"),
-    },
-    {
-      key = "i",
-      mods = "CTRL|SHIFT",
-      action = Act.RotatePanes("CounterClockwise"),
-    },
-    {
-      key = "<",
-      mods = "CTRL|SHIFT",
-      action = Act.ActivateTabRelative(-1),
-    },
-    {
-      key = ">",
-      mods = "CTRL|SHIFT",
-      action = Act.ActivateTabRelative(1),
-    },
-    {
-      key = "d",
-      mods = "CTRL|SHIFT",
-      action = Act.CloseCurrentPane({ confirm = true }),
-    },
-    {
-      key = "w",
-      mods = "CTRL|SHIFT",
-      action = Act.CloseCurrentTab({ confirm = true }),
-    },
-    {
-      key = "y",
-      mods = "CTRL|SHIFT",
-      action = Act.ActivateCopyMode,
-    },
-    {
-      key = "b",
-      mods = "CTRL|SHIFT",
-      action = toggle_domain("bg"),
-    },
-    {
-      key = "p",
-      mods = "CTRL|SHIFT",
-      action = Act.ActivateCommandPalette,
-    },
+    { key = "<", mods = "CTRL|SHIFT", action = Act.ActivateTabRelative(-1) },
+    { key = ">", mods = "CTRL|SHIFT", action = Act.ActivateTabRelative(1) },
+    { key = "_", mods = "CTRL|SHIFT", action = Act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "b", mods = "CTRL|SHIFT", action = toggle_domain("bg") },
+    { key = "c", mods = "CTRL|SHIFT", action = Act.CopyTo("Clipboard") },
+    { key = "d", mods = "CTRL|SHIFT", action = Act.CloseCurrentPane({ confirm = true }) },
+    { key = "f", mods = "CTRL|SHIFT", action = Act.Search({ CaseInSensitiveString = "" }) },
+    { key = "i", mods = "CTRL|SHIFT", action = Act.RotatePanes("CounterClockwise") },
+    { key = "m", mods = "CTRL|SHIFT", action = Act.TogglePaneZoomState },
+    { key = "n", mods = "CTRL|SHIFT", action = Act.SpawnTab("CurrentPaneDomain") },
+    { key = "o", mods = "CTRL|SHIFT", action = Act.RotatePanes("Clockwise") },
+    { key = "p", mods = "CTRL|SHIFT", action = Act.ActivateCommandPalette },
+    { key = "v", mods = "CTRL|SHIFT", action = Act.PasteFrom("Clipboard") },
+    { key = "w", mods = "CTRL|SHIFT", action = Act.CloseCurrentTab({ confirm = true }) },
+    { key = "y", mods = "CTRL|SHIFT", action = Act.ActivateCopyMode },
+    { key = "|", mods = "CTRL|SHIFT", action = Act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "c", mods = "ALT", action = Act.CopyTo("PrimarySelection") },
+    { key = "v", mods = "ALT", action = Act.PasteFrom("PrimarySelection") },
     table.unpack(SplitKeys),
   },
+  key_tables = key_tables,
 }
