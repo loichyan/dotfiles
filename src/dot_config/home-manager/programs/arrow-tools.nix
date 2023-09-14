4931b1551758d121db073321889a1f221e46a5fd
loichyan <loichyan@foxmail.com>
2023-09-14 16:52:20 +0800

    feat(home-manager): add `arrow-tools`

--- src/dot_config/home-manager/programs/arrow-tools.nix
+++ src/dot_config/home-manager/programs/arrow-tools.nix
@@ -11,9 +11,23 @@ let
       };
       cargoHash = "sha256-j4SCFwz5KD71tpSU3eYBY6+N5aiVhFIpw9rcJPcCbec=";
     };
+  arrow-tools =
+    rustPlatform.buildRustPackage rec {
+      pname = "arrow-tools";
+      version = "0.13.0";
+      src = fetchFromGitHub {
+        owner = "domoritz";
+        repo = "arrow-tools";
+        rev = "v${version}";
+        sha256 = "sha256-xKjFI3ZtlpIkH94U90NP/SHJK3RX2y5jNmYDi4BweqE=";
+      };
+      cargoHash = "sha256-6yjNjBC2w0dx/8QqFvaljQJAP0dWLnNur9023HeZ2e4=";
+      cargoBuildFlags = [ "--all" ];
+    };
 in
 {
   home.packages = [
+    arrow-tools
     datafusion-cli
     pkgs.pqrs
   ];
