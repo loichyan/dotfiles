65f5b17eb705a2078debd2c5803bcb5183925661
loichyan <loichyan@foxmail.com>
2023-09-19 09:51:19 +0800

    fix(fish): v3.3.1 compatible

--- src/dot_config/fish/functions/fish_user_key_bindings.fish
+++ src/dot_config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
-    set -l esc (bind -M insert \e | string match -gr '\'(.*)\'$')
+    set -l esc (bind -M insert \e | string match -r '\'(.*)\'$')[2]
     fish_default_key_bindings -M insert
     bind -M insert JK "$esc"
     bind -M insert \ce end-of-line
