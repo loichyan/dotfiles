e6a52e4780d9ecf844a0f7c6466561960806f91c
loichyan <loichyan@foxmail.com>
2023-09-26 15:35:33 +0800

    fix(fish): more checks to setup GunPG

--- src/dot_config/fish/conf.d/09_gpg.fish
+++ src/dot_config/fish/conf.d/09_gpg.fish
@@ -1,8 +1,11 @@
 if status is-login
     # Use gpg-agent as default
     if test -d ~/.gnupg && type -q gpgconf && type -q gpg-connect-agent
-        export GPG_TTY=(tty)
-        export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
-        echo UPDATESTARTUPTTY | gpg-connect-agent 1>/dev/null
+        set -e SSH_AGENT_PID
+        if test -z $gnupg_SSH_AUTH_SOCK_BY; or test $gnupg_SSH_AUTH_SOCK_BY -ne $fish_pid
+            set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+        end
+        set -gx GPG_TTY (tty)
+        gpg-connect-agent updatestartuptty /bye >/dev/null
     end
 end
