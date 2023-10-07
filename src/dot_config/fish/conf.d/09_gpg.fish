731bb1a89b46ae50be7c46c0ee42fa783c4cc631
loichyan <loichyan@foxmail.com>
2023-10-07 14:44:05 +0800

    fix(fish): update tty in interactive shell

--- src/dot_config/fish/conf.d/09_gpg.fish
+++ src/dot_config/fish/conf.d/09_gpg.fish
@@ -1,10 +1,16 @@
-if status is-login
+if status is-login || status is-interactive
     # Use gpg-agent as default
-    if test -d ~/.gnupg && type -q gpgconf && type -q gpg-connect-agent
+    if test -d ~/.gnupg && type -q gpgconf
         set -e SSH_AGENT_PID
         if test -z $gnupg_SSH_AUTH_SOCK_BY; or test $gnupg_SSH_AUTH_SOCK_BY -ne $fish_pid
             set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
         end
+    end
+end
+
+if status is-interactive
+    # Use gpg-agent as default
+    if test -d ~/.gnupg && type -q gpg-connect-agent
         set -gx GPG_TTY (tty)
         gpg-connect-agent updatestartuptty /bye >/dev/null
     end
