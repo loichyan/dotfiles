d9d0e37145e9492d8ac2d06b216120375fc8e73e
loichyan <loichyan@foxmail.com>
2023-10-07 14:22:33 +0800

    fix(nvim): migrate `indent-blankline` to v3

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -24,13 +24,20 @@ return {
     end,
   },
   {
-    "indent-blankline.nvim",
+    "lukas-reineke/indent-blankline.nvim",
     opts = {
-      use_treesitter = true,
-      show_current_context = true,
-      show_first_indent_level = false,
-      show_trailing_blankline_indent = false,
+      scope = {
+        enabled = true,
+        -- include = { node_type = { ["*"] = { "*" } } },
+        show_start = false,
+        show_end = false,
+      },
     },
+    config = function(_, opts)
+      require("ibl").setup(opts)
+      local hooks = require("ibl.hooks")
+      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+    end,
   },
   { "mini.indentscope", enabled = false },
   { "noice.nvim", enabled = false },
