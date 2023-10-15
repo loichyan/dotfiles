a3be7734f753e0a14829c256cc6da6b729f66e6d
loichyan <loichyan@foxmail.com>
2023-10-15 14:58:57 +0800

    feat(home-manager): add nightly cargo

--- src/dot_config/home-manager/programs/cargo-nightly-tools.nix
+++ src/dot_config/home-manager/programs/cargo-nightly-tools.nix
@@ -1,11 +1,25 @@
 { pkgs, ... }:
 let
-  inherit (pkgs) writeShellApplication fenix-monthly cargo-expand cargo-udeps;
+  inherit (pkgs)
+    writeShellApplication
+    fenix-monthly
+    cargo-expand
+    cargo-udeps
+    cargo-watch;
+  rustToolchain = fenix-monthly.default.toolchain;
+  cargo-nightly =
+    writeShellApplication {
+      name = "cargo-nightly";
+      runtimeInputs = [ rustToolchain ];
+      text = ''
+        cargo "$@"
+      '';
+    };
   cargo-nightly-expand =
     writeShellApplication {
       name = "cargo-nightly-expand";
       runtimeInputs = [
-        fenix-monthly.minimal.toolchain
+        rustToolchain
         cargo-expand
       ];
       text = ''
@@ -16,17 +30,30 @@ let
     writeShellApplication {
       name = "cargo-nightly-udeps";
       runtimeInputs = [
-        fenix-monthly.minimal.toolchain
+        rustToolchain
         cargo-udeps
       ];
       text = ''
         cargo udeps "$@"
       '';
     };
+  cargo-nightly-watch =
+    writeShellApplication {
+      name = "cargo-nightly-watch";
+      runtimeInputs = [
+        rustToolchain
+        cargo-watch
+      ];
+      text = ''
+        cargo watch "$@"
+      '';
+    };
 in
 {
   home.packages = [
+    cargo-nightly
     cargo-nightly-expand
     cargo-nightly-udeps
+    cargo-nightly-watch
   ];
 }
