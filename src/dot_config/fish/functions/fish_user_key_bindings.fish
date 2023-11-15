247d5e90f6e4350b346d601d6eea5dd5f7ec96fa
loichyan <loichyan@foxmail.com>
2023-11-15 10:27:55 +0800

    feat: use `jj` to escape from insert mode

--- src/dot_config/fish/functions/fish_user_key_bindings.fish
+++ src/dot_config/fish/functions/fish_user_key_bindings.fish
@@ -2,11 +2,12 @@ function fish_user_key_bindings
     fish_vi_key_bindings
     set -l esc (bind -M insert \e | string match -r '\'(.*)\'$')[2]
     fish_default_key_bindings -M insert
+    bind -M insert JJ "$esc"
     bind -M insert JK "$esc"
     bind -M insert \ce end-of-line
     bind -M insert \cw backward-kill-path-component
     bind -M insert \cb backward-kill-word
-    bind -M insert \cf forward-word
+    bind -M insert \cf forward-bigword
 
     if type -q fzf
         function fzf-history-widget -d "Show command history"
