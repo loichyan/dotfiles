c726fcaf1809bdc208f86bb1ae86bf559bb073e7
loichyan <loichyan@foxmail.com>
2024-01-14 16:27:32 +0800

    fix(home-manager): use master branch

--- src/dot_config/home-manager/flake.nix
+++ src/dot_config/home-manager/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     fenix = {
