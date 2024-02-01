8887118435c81ab4a9441d30c4457d5976e9064c
Loi Chyan <loichyan@foxmail.com>
2024-02-01 20:20:11 +0800

    fix(nvim): remove Nerd Fonts hack for `neo-tree.nvim`

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -14,39 +14,6 @@ return {
   {
     "neo-tree.nvim",
     opts = {
-      -- HACK: https://github.com/nvim-neo-tree/neo-tree.nvim/pull/909
-      default_component_configs = {
-        icon = {
-          folder_empty = "󰜌",
-          folder_empty_open = "󰜌",
-        },
-        git_status = {
-          symbols = {
-            renamed = "󰁕",
-            unstaged = "󰄱",
-          },
-        },
-      },
-      document_symbols = {
-        kinds = {
-          File = { icon = "󰈙", hl = "Tag" },
-          Namespace = { icon = "󰌗", hl = "Include" },
-          Package = { icon = "󰏖", hl = "Label" },
-          Class = { icon = "󰌗", hl = "Include" },
-          Property = { icon = "󰆧", hl = "@property" },
-          Enum = { icon = "󰒻", hl = "@number" },
-          Function = { icon = "󰊕", hl = "Function" },
-          String = { icon = "󰀬", hl = "String" },
-          Number = { icon = "󰎠", hl = "Number" },
-          Array = { icon = "󰅪", hl = "Type" },
-          Object = { icon = "󰅩", hl = "Type" },
-          Key = { icon = "󰌋", hl = "" },
-          Struct = { icon = "󰌗", hl = "Type" },
-          Operator = { icon = "󰆕", hl = "Operator" },
-          TypeParameter = { icon = "󰊄", hl = "Type" },
-          StaticMethod = { icon = "󰠄 ", hl = "Function" },
-        },
-      },
       window = {
         mappings = { ["<CR>"] = open_and_quit },
       },
