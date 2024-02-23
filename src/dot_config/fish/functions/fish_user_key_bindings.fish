48bf7967a9abf6e45267d3d0a3f8546370391cf9
Loi Chyan <loichyan@foxmail.com>
2024-02-23 14:54:14 +0800

    feat(fish): add keybindings to copy to clipboard

--- src/dot_config/fish/functions/fish_user_key_bindings.fish
+++ src/dot_config/fish/functions/fish_user_key_bindings.fish
@@ -16,6 +16,8 @@ function fish_user_key_bindings
     bind -M insert \cw backward-kill-path-component
     bind -M insert \cb backward-kill-word
     bind -M insert \cf forward-bigword
+    bind -M default yy fish_clipboard_copy
+    bind -M default Y fish_clipboard_copy
 
     if type -q fzf
         function fzf-history-widget -d "Show command history"
