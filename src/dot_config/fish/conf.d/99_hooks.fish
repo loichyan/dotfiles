bac6537fa09c7c77b440558982c4c323473a9add
Loi Chyan <loichyan@foxmail.com>
2024-03-15 17:45:51 +0800

    feat(fish): ignore histories of `chmod`

--- src/dot_config/fish/conf.d/99_hooks.fish
+++ src/dot_config/fish/conf.d/99_hooks.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     set -g __hist_ignored \
-        '^(;|\./|ADD|DEL|cat|cd|cp|echo|history|ll|ls|mkdir|mv|printf|rm|sudo|wl-copy|wl-paste).*'
+        '^(;|\./|ADD|DEL|cat|cd|chmod|cp|echo|history|ll|ls|mkdir|mv|printf|rm|sudo|wl-copy|wl-paste).*'
     set -g __hist_protected $history
     set -g __hist_failed
     set -g __hist_deletions
