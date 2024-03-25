9d8e5297bddd09e53b59f8310c2c420495b0aa5f
Loi Chyan <loichyan@foxmail.com>
2024-03-25 22:05:58 +0800

    refactor(fish): simplify fish hooks

--- src/dot_config/fish/conf.d/99_hooks.fish
+++ src/dot_config/fish/conf.d/99_hooks.fish
@@ -1,57 +1,6 @@
 if status is-interactive
-    set -g __hist_ignored \
-        '^(;|\./|ADD|DEL|cat|cd|chmod|cp|echo|history|ll|ls|mkdir|mv|printf|rm|sudo|wl-copy|wl-paste).*'
-    set -g __hist_protected $history
-    set -g __hist_failed
-    set -g __hist_deletions
-    set -g __hist
-    set -g __hist_last
-
-    function ADD -d "Protect a history"
-        set -l hist (string sub -s 5 $__hist)
-        if test -z "$hist"
-            set -a __hist_protected $__hist_last
-        else
-            set -a __hist_protected $hist
-        end
-        return 0
-    end
-
-    function DEL -d "Delete a history"
-        set -l hist (string sub -s 5 $__hist)
-        if test -z "$hist"
-            set -a __hist_deletions $__hist_last
-        else
-            set -a __hist_deletions $hist
-        end
-        return 0
-    end
-
-    function __hook_fish_preexec -e fish_preexec
-        set __hist (string trim -c ' ' $argv[1] | string collect)
-        # Manually trigger exit hooks before `exec` a command
-        if test (string sub -e 4 $__hist) = exec
-            __hook_fish_exit
-        end
-    end
-
-    function __hook_fish_postexec -e fish_postexec
-        set -l exitcode $status
-        set __hist_last $__hist
-        if test "$exitcode" = 0 && ! string match -qr $__hist_ignored $__hist
-            set -a __hist_protected $__hist
-        else
-            set -a __hist_failed $__hist
-        end
-    end
-
     function __hook_fish_exit -e fish_exit
-        for hist in $__hist_failed
-            if ! contains $hist $__hist_protected
-                set -a __hist_deletions $hist
-            end
-        end
-        history delete -Ce '' $__hist_deletions
+        echo all | history delete --prefix ';' >/dev/null
     end
 
     if type -q direnv
