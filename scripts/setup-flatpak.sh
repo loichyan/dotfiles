80dc7da1bd8b319d5debc57dbc858f29d55ba42e
Loi Chyan <loichyan@foxmail.com>
2024-04-27 18:52:28 +0800

    feat: update Gnome configuration

---
+++ scripts/setup-flatpak.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install -y "org.fedoraproject.Platform//f$(rpm -E %fedora)" \
+  org.gnome.Calculator \
+  org.gnome.Calendar \
+  org.gnome.Characters \
+  org.gnome.Connections \
+  org.gnome.Contacts \
+  org.gnome.Evince \
+  org.gnome.Extensions \
+  org.gnome.Logs \
+  org.gnome.Loupe \
+  org.gnome.Maps \
+  org.gnome.NautilusPreviewer \
+  org.gnome.Snapshot \
+  org.gnome.TextEditor \
+  org.gnome.Totem \
+  org.gnome.Weather \
+  org.gnome.baobab \
+  org.gnome.clocks \
+  org.gnome.font-viewer \
+  org.mozilla.Thunderbird
