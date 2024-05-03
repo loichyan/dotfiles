1d2f05a792fe7a6613c6dc3136dc8012b214f7bc
Loi Chyan <loichyan@foxmail.com>
2024-05-03 09:11:08 +0800

    fix(fish): confirm to delete histories

--- src/dot_config/fish/functions/history_delete.fish
+++ src/dot_config/fish/functions/history_delete.fish
@@ -1,6 +1,6 @@
 function history_delete -d "Delete history entries"
     if test -z "$argv" || ! argparse prefix=+ -- $argv
-        echo -n "\
+        echo -n '\
 USAGE:
 
 history_delete [OPTION]...
@@ -8,14 +8,24 @@ history_delete [OPTION]...
 OPTION:
 
 -p/--prefix  Search histories by prefix
-"
+'
         return 1
     end
 
     for pref in $_flag_prefix
-        echo all | history delete -C --prefix $pref
         if type -q atuin
-            atuin search --delete --search-mode prefix $pref
+            atuin search -f '{command}' --search-mode prefix $pref
+        else
+            history search -C --prefix $pref
+        end
+
+        read -l -P "Confirm to delete? [Yes/no]" confirm
+        switch (string lower $confirm)
+            case y yes ''
+                echo all | history delete -C --prefix $pref &>/dev/null
+                if type -q atuin
+                    atuin search --delete --search-mode prefix $pref &>/dev/null
+                end
         end
     end
 end
