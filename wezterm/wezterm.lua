a495b130504ff1e81830373eae5d7ce2872f6a78
Loi Chyan <loichyan@foxmail.com>
2024-07-29 17:21:15 +0800

    fix(wezterm): add clipboard keybindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -57,5 +57,9 @@ return {
   --keys = require("config.keys"),
   --key_tables = require("config.key_tables"),
   default_prog = { "tmux", "new", "-A" },
+  keys = {
+    { key = "c", mods = "CTRL|SHIFT", action = { CopyTo = "Clipboard" } },
+    { key = "v", mods = "CTRL|SHIFT", action = { PasteFrom = "Clipboard" } },
+  },
   enable_csi_u_key_encoding = true,
 }
