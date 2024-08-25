119f2ac8480a5165d6ff31ebec3ff37a49021d39
Loi Chyan <loichyan@foxmail.com>
2024-08-25 19:45:24 +0800

    fix(tmux): make tmux-continuum autosave work properly

--- src/dot_config/home-manager/services/tmux.nix
+++ src/dot_config/home-manager/services/tmux.nix
@@ -2,29 +2,26 @@
 let
   inherit (pkgs) tmux writeShellApplication;
 
-  mkTmuxService =
-    name:
-    let
-      tmux-start = writeShellApplication {
-        name = "tmux-start";
-        # make sure tmux is in $PATH
-        runtimeInputs = [ tmux ];
-        text = "tmux -L ${name} new-session -s main -d";
-      };
-    in
-    {
+  tmux-start-server = writeShellApplication {
+    name = "tmux-start-server";
+    # make sure tmux is in $PATH
+    runtimeInputs = [ tmux ];
+    text = "tmux new-session -ds main";
+  };
+in
+{
+  systemd.user.services = {
+    tmux-server = {
       Unit = {
-        Description = "tmux server (${name})";
+        Description = "tmux server";
       };
       # adapted from: https://github.com/tmux-plugins/tmux-continuum/blob/0698e8f4b17d/scripts/handle_tmux_automatic_start/systemd_enable.sh#L20
       Service = {
         Type = "forking";
-
-        WorkingDirectory = pkgs.myData.home;
-        ExecStart = "${tmux-start}/bin/tmux-start";
-
-        ExecStop = "${tmux}/bin/tmux -L ${name} kill-server";
         KillMode = "control-group";
+
+        ExecStart = "${tmux-start-server}/bin/tmux-start-server";
+        ExecStop = "${tmux}/bin/tmux kill-server";
 
         RestartSec = 2;
       };
@@ -32,10 +29,5 @@ let
         WantedBy = [ "default.target" ];
       };
     };
-in
-{
-  systemd.user.services = {
-    tmux-default = mkTmuxService "default";
-    tmux-popup = mkTmuxService "popup";
   };
 }
