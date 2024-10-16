c7ecba3dee0a34bf37c3475478cfe9e7ac984e59
Loi Chyan <loichyan@foxmail.com>
2024-10-16 21:17:31 +0800

    feat(fish): support `cargo_doc_server [--nightly | --cfg-docsrs]`

--- src/dot_config/fish/functions/cargo_doc_server.fish
+++ src/dot_config/fish/functions/cargo_doc_server.fish
@@ -1,5 +1,5 @@
 function cargo_doc_server -d "Start a server for Cargo documentation"
-    if test -z "$argv" || ! argparse "p/port=" -- $argv
+    if test -z "$argv" || ! argparse nightly cfg-docsrs "p/port=" -- $argv
         echo -n "\
 USAGE:
 
@@ -8,13 +8,23 @@ cargo_doc_server -p <int>
 OPTION:
 
 -p/--port <int>  Port to listen on
+--nightly        Use nightly Rust toolchain
+--cfg-docsrs     Set `docsrs` cfg flag
 "
         return 1
     end
 
     set -l port $_flag_port
+    set -l cargo_watch (set -q _flag_nightly && echo "cargo-nightly-watch" || echo "cargo-watch")
+    set -l rustflags
+
+    if set -q _flag_cfg_docsrs
+        set -a rustflags --cfg docsrs
+    end
+
+
     fish -c "
-    cargo-nightly-watch -E RUSTFLAGS='--cfg docsrs' -x 'doc $argv' &
+    $cargo_watch -E RUSTFLAGS='$rustflags' -x 'doc $argv' &
     mkdir -p target
     dufs -p $port[1] --allow-search --enable-cors --render-spa --render-try-index target
     "
