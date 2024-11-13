cafbb1c384ac306c8a7efb3c133dd73c5f8fa5e8
Loi Chyan <loichyan@foxmail.com>
2024-11-13 17:19:13 +0800

    fix(packages): fix build failure of cargo-llvm-cov

--- src/dot_config/home-manager/programs/cargo-nightly-tools.nix
+++ src/dot_config/home-manager/programs/cargo-nightly-tools.nix
@@ -23,22 +23,35 @@ let
     '';
   };
   cargoCommand =
-    { name }:
+    {
+      name,
+      pkg ? pkgs."cargo-${name}",
+    }:
     writeShellApplication {
       name = "cargo-nightly-${name}";
       runtimeInputs = [
         rustToolchain
-        pkgs."cargo-${name}"
+        pkg
       ];
       text = ''cargo ${name} "$@"'';
     };
+  # FIXME: use pkgs.cargo-llvm-cov when https://github.com/NixOS/nixpkgs/pull/353741 is released
+  inherit
+    (import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/fc6278d271c7e23367c9525356e354179b467d3f.tar.gz";
+    }) { })
+    cargo-llvm-cov
+    ;
 in
 {
   home.packages = [
     cargo-nightly
     rustfmt-nightly
     (cargoCommand { name = "expand"; })
-    (cargoCommand { name = "llvm-cov"; })
+    (cargoCommand {
+      name = "llvm-cov";
+      pkg = cargo-llvm-cov;
+    })
     (cargoCommand { name = "udeps"; })
     (cargoCommand { name = "watch"; })
   ];
