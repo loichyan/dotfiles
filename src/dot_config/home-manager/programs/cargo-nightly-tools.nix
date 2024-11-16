63419758632c453d2e0c67f65e7e12bb04f68672
Loi Chyan <loichyan@foxmail.com>
2024-11-16 15:20:51 +0800

    fix(packages): switch back to nixpkgs-unstable's cargo-llvm-cov

--- src/dot_config/home-manager/programs/cargo-nightly-tools.nix
+++ src/dot_config/home-manager/programs/cargo-nightly-tools.nix
@@ -35,23 +35,13 @@ let
       ];
       text = ''cargo ${name} "$@"'';
     };
-  # FIXME: use pkgs.cargo-llvm-cov when https://github.com/NixOS/nixpkgs/pull/353741 is released
-  inherit
-    (import (builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/fc6278d271c7e23367c9525356e354179b467d3f.tar.gz";
-    }) { })
-    cargo-llvm-cov
-    ;
 in
 {
   home.packages = [
     cargo-nightly
     rustfmt-nightly
     (cargoCommand { name = "expand"; })
-    (cargoCommand {
-      name = "llvm-cov";
-      pkg = cargo-llvm-cov;
-    })
+    (cargoCommand { name = "llvm-cov"; })
     (cargoCommand { name = "udeps"; })
     (cargoCommand { name = "watch"; })
   ];
