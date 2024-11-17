bb17654dbe77ad808a6952646488f14c2a440ae4
Loi Chyan <loichyan@foxmail.com>
2024-11-17 14:50:36 +0800

    fix(templates): use stable Rust toolchain for development

--- src/dot_config/home-manager/templates/rust/flake.nix
+++ src/dot_config/home-manager/templates/rust/flake.nix
@@ -1,12 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    #ra-flake.url = "github:loichyan/ra-flake";
   };
 
   outputs =
@@ -18,27 +17,21 @@
           inherit system;
           overlays = [
             inputs.fenix.overlays.default
-            #inputs.ra-flake.overlays.default
           ];
         };
         inherit (pkgs)
           lib
           mkShell
           fenix
-          #ra-flake
           ;
 
-        # Rust toolchain
-        crate = (lib.importTOML ./Cargo.toml).package;
-        rustChannel = {
-          channel = crate.rust-version;
-          sha256 = "";
-        };
-        rustToolchain = fenix.toolchainOf rustChannel;
-        # Additional targets
-        #rustWasmToolChain = fenix.targets.wasm32-unknown-unknown.toolchainOf rustChannel;
+        cargoManifest = (lib.importTOML ./Cargo.toml);
+        crate = cargoManifest.package;
 
-        # For development
+        # Rust toolchain for development
+        rustToolchain = fenix.stable;
+        # additional targets
+        #rustWasmToolChain = fenix.targets.wasm32-unknown-unknown.stable;
         rust-dev = fenix.combine (
           with rustToolchain;
           [
@@ -47,14 +40,15 @@
             #rustWasmToolChain.rust-std
           ]
         );
-        rust-analyzer = rustToolchain.rust-analyzer;
-        # rust-analyzer is not available before Rust 1.64
-        #rust-analyzer = ra-flake.make {
-        #  version.rust = crate.rust-version;
-        #  sha256 = "";
-        #};
 
-        # For building packages
+        # Rust toolchain of MSRV
+        rust-msrv =
+          (fenix.toolchainOf {
+            channel = crate.rust-version;
+            sha256 = "";
+          }).minimalToolchain;
+
+        # Rust toolchain to build packages
         rust-minimal = rustToolchain.minimalToolchain;
         rustPlatform = pkgs.makeRustPlatform {
           cargo = rust-minimal;
@@ -83,8 +77,11 @@
         devShells.with-rust-analyzer = mkShell {
           packages = [
             rust-dev
-            rust-analyzer
+            rustToolchain.rust-analyzer
           ];
+        };
+        devShells.msrv = mkShell {
+          packages = [ rust-msrv ];
         };
       }
     );
