018de307e3c462a9592e428fb581153e7874a755
Loi Chyan <loichyan@foxmail.com>
2024-11-19 08:20:48 +0800

    fix(fish): quote variables in test expressions

--- src/dot_config/fish/config.fish
+++ src/dot_config/fish/config.fish
@@ -1,7 +1,7 @@
 set -gx __fish_did_init 1
 
 if status is-interactive
-    if test $MY_PROXY_ENABLED = true
+    if test "$MY_PROXY_ENABLED" = true
         setproxy $MY_HTTP_PROXY
     end
     if type -q zoxide
