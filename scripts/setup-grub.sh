028bb9dfc5c692ebf6dc34b37419a3b7b60cda17
Loi Chyan <loichyan@foxmail.com>
2024-11-25 06:18:36 +0800

    fix(scripts): de-duplicate grub2 entries

--- scripts/setup-grub.sh
+++ scripts/setup-grub.sh
@@ -2,17 +2,24 @@
 
 set -euxo pipefail
 
+if [[ -f /etc/default/grub ]]; then
+	sudo cp /etc/default/grub /etc/default/grub-backup
+fi
+# Remove duplicate Fedora entries
+# c.f. https://discussion.fedoraproject.org/t/duplicated-grub-entries-on-silverblue-kinoite/33882/5
+cat <<-CONF | sudo tee /etc/default/grub
+	GRUB_TIMEOUT=5
+	GRUB_DISTRIBUTOR="\$(sed 's, release .*\$,,g' /etc/system-release)"
+	GRUB_DEFAULT=saved
+	GRUB_DISABLE_SUBMENU=true
+	GRUB_TERMINAL_OUTPUT=gfxterm
+	GRUB_CMDLINE_LINUX="rhgb quiet"
+	GRUB_DISABLE_RECOVERY=true
+	GRUB_ENABLE_BLSCFG=true
+CONF
+
 tempdir="$(mktemp -d)"
 trap 'rm $tempdir' EXIT
 git clone https://github.com/vinceliuice/grub2-themes "$tempdir"
 cd "$tempdir"
 sudo ./install.sh -b -t stylish
-# Remove duplicate Fedora entries
-# c.f. https://discussion.fedoraproject.org/t/duplicated-grub-entries-on-silverblue-kinoite/33882/5
-if [ -d /sys/firmware/efi ]; then
-	sudo grub2-switch-to-blscfg
-	sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
-else
-	sudo touch /boot/grub2/.grub2-blscfg-supported
-	sudo grub2-mkconfig -o /boot/grub2/grub.cfg
-fi
