30737643a478478a0f7903ef8e57acc3eac26291
Loi Chyan <loichyan@foxmail.com>
2025-01-05 08:39:52 +0000

    refactor(bash): load minimal configurations for Bash

--- src/dot_bashrc.d/10_gpg.sh
+++
@@ -1,8 +0,0 @@
-# c.f. https://wiki.archlinux.org/title/GnuPG#SSH_agent
-unset SSH_AGENT_PID
-if [[ $gnupg_SSH_AUTH_SOCK_BY != "$$" ]]; then
-	export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-fi
-
-export GPG_TTY="$(tty)"
-gpg-connect-agent --quiet updatestartuptty /bye >/dev/null
