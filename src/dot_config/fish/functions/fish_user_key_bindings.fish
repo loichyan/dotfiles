87b6f9b89c52be91b53569c547fb916983c4d1dd
Loi Chyan <loichyan@foxmail.com>
2025-01-11 10:15:50 +0800

    fix(fish): force to handle VI cursor shape after editing command

--- src/dot_config/fish/functions/fish_user_key_bindings.fish
+++ src/dot_config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,5 @@
 function fish_user_key_bindings
-    function __bind_i_escape
+    function fish_escape_insert_mode
         if commandline -P
             commandline -f cancel
         else
@@ -10,14 +10,18 @@ function fish_user_key_bindings
 
     fish_vi_key_bindings
     fish_default_key_bindings -M insert
-    bind -M insert \e __bind_i_escape
-    bind -M insert jk __bind_i_escape
+    bind -M insert \e fish_escape_insert_mode
+    bind -M insert jk fish_escape_insert_mode
     bind -M insert \ce end-of-line
     bind -M insert \cw backward-kill-path-component
     bind -M insert \cb backward-kill-word
     bind -M insert \cf forward-bigword
     bind -M default yy fish_clipboard_copy
     bind -M default Y fish_clipboard_copy
+
+    # Manually trigger postexec event to force Fish to handle VI cursor
+    bind -M insert \ee 'edit_command_buffer; emit fish_postexec'
+    bind -M insert \ev 'edit_command_buffer; emit fish_postexec'
 
     if ! type -q atuin && type -q fzf
         function fzf-history-widget -d "Show command history"
