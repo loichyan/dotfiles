8552f85b8ea8517dce09f15643f742d1390d0e00
Loi Chyan <loichyan@foxmail.com>
2025-01-11 10:15:50 +0800

    fix(fish): force to handle VI cursor shape after editing command

--- src/dot_config/fish/conf.d/11_config.fish
+++ src/dot_config/fish/conf.d/11_config.fish
@@ -3,11 +3,7 @@ if status is-interactive
     set -g fish_greeting
 
     # Make cursor distinct among modes
-    set -g fish_vi_force_cursor 1
-    set -g fish_cursor_default block
-    set -g fish_cursor_insert line blink
-    set -g fish_cursor_visual block
-    set -g fish_cursor_replace_one underscore
+    fish_vi_cursor
 
     # Add vendored completions and functions from Nixpkgs
     for p in (string split ' ' -- $NIX_PROFILES)
