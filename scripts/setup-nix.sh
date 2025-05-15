b0ada3b7b36e5d73b2b998955538c8ac588dbbe9
Loi Chyan <loichyan@foxmail.com>
2025-05-15 01:45:59 +0000

    feat(scripts): add single-user mode Nix installation script

--- scripts/setup-nix.sh
+++ scripts/setup-nix.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
-
 set -euxo pipefail
 
-# Install Nix
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+multiuser() {
+	# Install Nix
+	curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
-# Forward proxy variables
-# shellcheck disable=2154
-# TODO: remove this workaround if https://github.com/DeterminateSystems/nix-installer/issues/974 fixed
-cat <<-INI | sudo tee /etc/systemd/system/nix-daemon.service.d/override.conf
-	[Service]
-	Environment="http_proxy=$http_proxy"
-	Environment="https_proxy=$https_proxy"
-	Environment="no_proxy=$no_proxy"
-INI
+	# Forward proxy variables
+	# shellcheck disable=2154
+	# TODO: remove this workaround if https://github.com/DeterminateSystems/nix-installer/issues/974 fixed
+	cat <<-INI | sudo tee /etc/systemd/system/nix-daemon.service.d/override.conf
+		[Service]
+		Environment="http_proxy=$http_proxy"
+		Environment="https_proxy=$https_proxy"
+		Environment="no_proxy=$no_proxy"
+	INI
 
-# Activate Home Manager
-nix run home-manager/master -- switch --impure
+	# Activate Home Manager
+	nix run home-manager/master -- switch --impure
+
+}
+
+singleuser() {
+	curl -L https://nixos.org/nix/install | sh -s -- --no-daemon --no-channel-add --no-modify-profile
+}
