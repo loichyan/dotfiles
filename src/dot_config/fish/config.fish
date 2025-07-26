9d43b257542d5412416e3ac4542194313a79ba00
Loi Chyan <loichyan@foxmail.com>
2025-07-26 00:57:18 +0000

    fix(fish): let Atuin keybindings take effect
    Fzf keybindings are loaded anyway from the vendored conf dirs.

--- src/dot_config/fish/config.fish
+++ src/dot_config/fish/config.fish
@@ -2,4 +2,21 @@ if status is-interactive
     if set -q MY_PROXY_ENABLED
         setproxy $MY_HTTP_PROXY
     end
+
+    if type -q atuin
+        atuin init fish --disable-up-arrow | source
+        if not ps -oargs x | string match -q 'atuin daemon'
+            rm -f ~/.local/share/atuin/atuin.sock
+            atuin daemon &>/dev/null & disown
+        end
+    end
+
+    if type -q direnv
+        set -g direnv_fish_mode eval_after_arrow
+        direnv hook fish | source
+    end
+
+    if type -q zoxide
+        zoxide init fish | source
+    end
 end
--- src/dot_config/fish/functions/fish_user_key_bindings.fish
+++ src/dot_config/fish/functions/fish_user_key_bindings.fish
@@ -23,15 +23,5 @@ function fish_user_key_bindings
     bind -M insert \ee 'edit_command_buffer; set fish_bind_mode $fish_bind_mode'
     bind -M insert \ev 'edit_command_buffer; set fish_bind_mode $fish_bind_mode'
 
-    if ! type -q atuin && type -q fzf
-        function fzf-history-widget -d "Show command history"
-            set -lx FZF_DEFAULT_OPTS "--height 50% $FZF_DEFAULT_OPTS --scheme=history --bind=ctrl-r:toggle-sort,ctrl-z:ignore +m"
-            history -z | eval fzf --read0 --print0 -q '(commandline)' | read -lz result
-            and commandline -- $result
-            commandline -f repaint
-        end
-        bind -M insert \cr fzf-history-widget
-    end
-
     set -g fish_sequence_key_delay_ms 100
 end
