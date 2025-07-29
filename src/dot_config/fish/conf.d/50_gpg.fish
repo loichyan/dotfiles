a65ca66c0661cc0089c4b906dde3bba3678b39f5
Loi Chyan <loichyan@foxmail.com>
2025-07-29 14:42:11 +0800

    docs: fix typos in comments

--- src/dot_config/fish/conf.d/50_gpg.fish
+++ src/dot_config/fish/conf.d/50_gpg.fish
@@ -1,11 +1,11 @@
 if set -q MY_GPG_ENABLED && type -q gpg
-    # Taken from https://wiki.archlinux.org/title/GnuPG#SSH_agent
+    # See <https://wiki.archlinux.org/title/GnuPG#SSH_agent>
     set -e SSH_AGENT_PID
     if test "$gnupg_SSH_AUTH_SOCK_BY" != $fish_pid
         set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
     end
 
-    # Configure pinentry to use the correct TTTY
+    # Configure pinentry to use the correct TTY
     set -gx GPG_TTY (tty)
     gpg-connect-agent -q updatestartuptty /bye >/dev/null
 end
