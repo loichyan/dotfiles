7d0f546974c93f1f7c70783963f6a38f033c295e
Loi Chyan <loichyan@foxmail.com>
2025-08-19 21:10:01 +0800

    feat(fish): enable colores for fish's builtin help pages

--- src/dot_config/fish/functions/man.fish
+++ src/dot_config/fish/functions/man.fish
@@ -1,17 +1,3 @@
 function man -d "man(1) with colors"
-    # Stolen from <https://github.com/ohmyzsh/ohmyzsh/blob/5c804257ceb5b3062b876afae290adf72c474aad/plugins/colored-man-pages/colored-man-pages.plugin.zsh#L9>
-    # bold & blinking mode
-    set -lx LESS_TERMCAP_mb (set_color red -o)
-    set -lx LESS_TERMCAP_md (set_color red -o)
-    set -lx LESS_TERMCAP_me (set_color normal)
-    # standout mode
-    set -lx LESS_TERMCAP_so (set_color -r bryellow -o)
-    set -lx LESS_TERMCAP_se (set_color normal)
-    # underlining
-    set -lx LESS_TERMCAP_us (set_color green -o)
-    set -lx LESS_TERMCAP_ue (set_color normal)
-
-    set -lx GROFF_NO_SGR 1 # TODO: fish's pager does not show colors
-    set -lx MANPAGER 'less -R'
-    command man $argv
+    colored_man_wrap (type -P man) $argv
 end
