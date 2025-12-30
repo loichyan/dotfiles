9ce9e47d026931d7958ac115ee7368592cd3cb54
Loi Chyan <loichyan@outlook.com>
2025-12-30 11:02:13 +0800

    feat(flatpak): update package set

--- scripts/setup-flatpak.sh
+++ scripts/setup-flatpak.sh
@@ -15,20 +15,17 @@ flatpak install -y \
 	org.gnome.Loupe \
 	org.gnome.Maps \
 	org.gnome.NautilusPreviewer \
+	org.gnome.Showtime \
 	org.gnome.Snapshot \
 	org.gnome.TextEditor \
-	org.gnome.Totem \
 	org.gnome.Weather \
 	org.gnome.baobab \
 	org.gnome.clocks \
-	org.gnome.font-viewer \
-	org.mozilla.Thunderbird \
-	com.github.tchx84.Flatseal
+	org.gnome.font-viewer
 
 # Additional packages from Flathub
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install -y app.zen_browser.zen md.obsidian.Obsidian
+flatpak install com.github.tchx84.Flatseal net.thunderbird.Thunderbird app.zen_browser.zen md.obsidian.Obsidian
 
+# Share nixpkgs and fontconfig
 flatpak override --user --filesystem=/nix:ro --filesystem=xdg-config/fontconfig:ro
-# Share Git configuration directory so that it can be recognized by obsidian-git
-flatpak override --user --filesystem=xdg-config/git:ro md.obsidian.Obsidian
